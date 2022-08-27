d3b8066c904e95e2ff8b54ade067fb66729d1ba3
KerryCooper <maxqofficial@foxmail.com>
2022-08-27 11:11:33 +0800

    script to convert m4a into wav with multiprocess

--- pre_process/m4a_to_wav.py
+++ pre_process/m4a_to_wav.py
@@ -3,47 +3,73 @@ Convert the source audio of the VOX2 dataset to WAV format
 pydub & pandas needed
 """
 
-from ast import arg
-from email.mime import audio
 from ntpath import join
-import os 
+import os
 import re
-import pandas as pd
-import argparse
 from pydub import AudioSegment
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--dir", required=True, help="root dir of vox dataset")
-parser.add_argument("-t", "--target", required=True, help="dir of converted dataset")
-parser.add_argument("-br", "--bitrate", help="sample rate", default=16000)
+def Sub_process(q):
+    while True:
+        end, origin_path, target_path, br = q.get()
+        if not end:
+            audio = AudioSegment.from_file(origin_path)
+            audio.export(target_path, format='wav', bitrate=br)
+        else:
+            break
 
-args = vars(parser.parse_args())
-dir = args["dir"]
-target = args["target"]
-br = args["bitrate"]
+if __name__ == "__main__":
+    import pandas as pd
+    import argparse
+    from multiprocessing import Process
+    from multiprocessing import Queue
 
-os.mkdir(target)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dir", required=True, help="root dir of vox dataset")
+    parser.add_argument("-t", "--target", required=True, help="dir of converted dataset")
+    parser.add_argument("-br", "--bitrate", help="sample rate", default=16000)
+    parser.add_argument("-p", "--process", help="Num of processes", default=4)
 
-info_list = []
+    args = vars(parser.parse_args())
+    dir = args["dir"]
+    target = args["target"]
+    br = args["bitrate"]
+    process = int(args["process"])
+
+    os.mkdir(target)
+
+    q = Queue(300)
+    process_list = []
+    for i in range(process):
+        p = Process(target=Sub_process,args=(q,))
+        p.start()
+        process_list.append(p)
+
+    info_list = []
+    g = os.walk(dir)  
+    for path,dir_list,file_list in g:
+        for file_name in file_list:  
+            if re.search(r'\.m4a$', file_name):
+                info = {}
+                info["speaker_id"] = re.search(r'id\d{5}', path).group()
+                info["utterance"] = '/'+file_name.split('.')[0]+".wav"
+
+                if not os.path.exists(target+info["speaker_id"]):
+                    os.mkdir(target+info["speaker_id"])
+
+                q.put((False, join(path, file_name), join(target,info["speaker_id"]+"\\"+info["utterance"]), br))
+                
+                info_list.append(info)
 
 
-g = os.walk(dir)  
-for path,dir_list,file_list in g:
-    for file_name in file_list:  
-        if re.search(r'\.m4a$', file_name):
-            info = {}
-            info["speaker_id"] = re.search(r'id\d{5}', path).group()
-            info["utterance"] = '/'+file_name.split('.')[0]+".wav"
+    # notify all threads to quit..
+    for i in range(process):
+        q.put((True, "", "", 0))
 
-            if not os.path.exists(target+info["speaker_id"]):
-                os.mkdir(target+info["speaker_id"])
+    for p in process_list:
+        p.join()
 
-            audio = AudioSegment.from_file(join(path, file_name))
-            audio.export(join(target,info["speaker_id"]+"\\"+info["utterance"]+".wav"), format='wav', bitrate=br)
-            
-            info_list.append(info)
-
-# save csv
-df = pd.DataFrame(info_list, columns=["speaker_id", "utterance"])
-df.to_csv(target+"utterance_info.csv")
-print("--done--")
+    # save csv
+    df = pd.DataFrame(info_list, columns=["speaker_id", "utterance"])
+    df.to_csv(target+"utterance_info.csv")
+    
+    print("--done--")
